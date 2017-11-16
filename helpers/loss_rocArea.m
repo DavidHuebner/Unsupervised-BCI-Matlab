a22237e84371dfe8f95dd08c95d11d947d8f1c34
David Hübner <david.huebner@blbt.uni-freiburg.de>
2017-11-16 13:37:55 +0100

    attributed functions from BBCI toolbox, copied MIT license

--- helpers/loss_rocArea.m
+++ helpers/loss_rocArea.m
@@ -13,8 +13,28 @@ function loss= loss_rocArea(label, out, varargin)
 %
 % Benjamin Blankertz
 %
-% Taken from the BBCI Toolbox:
+% Taken from the BBCI Toolbox distributed with a MIT License
 % https://github.com/bbci/bbci_public
+% 
+% Copyright (c) 2015 The BBCI Group
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
 
 if size(label,1)~=2,
   error('roc works only for 2-class problems');
